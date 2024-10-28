73b2a1155303c0997368c6d0370b2e35bdf77b82
developmentofstartup <cavididman@hotmail.com>
2024-10-28 15:14:56 +0400

    v1

---
+++ src/components/info/content.js
@@ -0,0 +1,36 @@
+const hardSkills = [
+    'HTML, CSS , JS (ES6+, DOM manipulation)',
+    'Web accessibility and SEO',
+    'React, Redux, Chakra UI, Formik, Yup, Material UI, Tailwind',
+    'Unit Testing: Jest, React Testing Library',
+    'Building CI/CD pipelines',
+    'RESTfull API with Node.js or .NET Core',
+    'Responsive web design using Figma',
+    'Version Control with Git',
+    'Languages: English, Russian, Turkish, Azerbaijani'
+];
+const softSkills = [
+    'High attention to detail',
+    'Continuous Learning',
+    'Good problem-solving skills',
+    'Analytical Thinking',
+    'Critical Thinking',
+    'Adaptability',
+    'Creativity',
+    'Communication'
+];
+const education = [
+    'Azerbaijan Technical University ·  Computer Science',
+    'Code Academy · Full Stack Web Developer',
+    'Meta (Coursera) · Advanced React',
+    'Meta (Coursera) · UI/UX Design'
+];
+const experiences = [
+    {
+        startDate: '2023 March',
+        endDate: '2024 February',
+        jobTitle: 'Front End Developer',
+        description: 'Developed UIs for various projects, including airline ticket sales, therapy session scheduling, educational course sales, and social media platforms. Initially joined as an intern for 2 months, then transitioned to a full-time developer. Over the past 4 months, served as a team leader, mentoring two developers, one of whom stayed on as a front-end developer. Gained proficiency in HTML, CSS, JavaScript, jQuery, and SCSS.'
+    }
+];
+export { hardSkills, softSkills, education, experiences }
---
+++ src/components/projects/content.js
@@ -0,0 +1,96 @@
+// images
+import img1 from '../../assets/projects/1.png';
+import img2 from '../../assets/projects/2.png';
+import img3 from '../../assets/projects/3.png';
+
+import prj2_imgPhone from '../../assets/projects/second_prj/android_lg.png';
+import prj2_imgTab from '../../assets/projects/second_prj/ipad.png';
+import prj2_imgMac from '../../assets/projects/second_prj/macbook.png';
+// tools
+import htmlIcon from '../../assets/tools/html5.svg';
+import cssIcon from '../../assets/tools/css3.svg';
+import bootstrapIcon from '../../assets/tools/bootstrap.svg';
+import reactIcon from '../../assets/tools/react.svg';
+import JQueryIcon from '../../assets/tools/JQuery.svg';
+import jsIcon  from '../../assets/tools/js.svg';
+import reduxIcon from '../../assets/tools/redux.svg';
+
+const content = [
+    {
+        imgSrc: {
+            laptopSize: img1,
+            tabletSize: img2,
+            mobileSize: img3
+        },
+        imgAlt: 'Screenshot of the Helply website',
+        liveLink: 'https://helply.ae/therapist',
+        codesLink: '',
+        designLink: '',
+        info: {
+            header: 'Helply',
+            descriptions: "Helply's goal is to provide users with personalized therapist matches tailored to their preferences and needs. Please note that the codes are not included as I do not have permission to share them. The design was crafted by my former co-worker.",
+            tools: [
+                {
+                    toolName: 'HTML',
+                    toolIcon: htmlIcon,
+                    iconColor: '#F08959'
+                },
+                {
+                    toolName: 'CSS',
+                    toolIcon: cssIcon,
+                    iconColor: '#51709F'
+                },
+                {
+                    toolName: 'JS',
+                    toolIcon: jsIcon,
+                    iconColor: '#EDD858'
+                },
+                {
+                    toolName: 'Boostrap',
+                    toolIcon: bootstrapIcon,
+                    iconColor: '#a020f0'
+                },
+                {
+                    toolName: 'JQuery',
+                    toolIcon: JQueryIcon,
+                    iconColor: '#0769ad'
+                }
+            ]
+        },
+    },
+
+    {
+        imgSrc: {
+            laptopSize: prj2_imgMac,
+            tabletSize: prj2_imgTab,
+            mobileSize: prj2_imgPhone
+        },
+        imgAlt: 'Screenshot of the BudgetBuddy website',
+        liveLink: 'https://developmentofstartup.github.io/BudgetBuddy/',
+        codesLink: 'https://github.com/developmentofstartup/BudgetBuddy/',
+        designLink: 'https://www.figma.com/design/Mrzptx7vVSkLlLct9V6JZG/BudgetBuddy?node-id=0-1&t=AEjJLbWSa0Y9JBIO-0',
+        info: {
+            header: 'BudgetBuddy',
+            descriptions: "BudgetBuddy is a user-friendly savings app designed to help individuals track their expenses and income. With intuitive features for adding and managing transactions, BudgetBuddy allows users to monitor their daily, weekly, and monthly spending habits. The app includes visual charts to provide insights into financial trends and aids users in achieving their savings goals. Currently, I'm working on the design, so other links are not yet provided.",
+            tools: [
+                {
+                    toolName: 'React',
+                    toolIcon: reactIcon,
+                    iconColor: '#61DAFB'
+                },
+                {
+                    toolName: 'Boostrap',
+                    toolIcon: bootstrapIcon,
+                    iconColor: '#a020f0'
+                },
+                {
+                    toolName: 'Redux',
+                    toolIcon: reduxIcon,
+                    iconColor: '#764ABC'
+                }
+            ]
+        },
+    },
+];
+
+export default content;
